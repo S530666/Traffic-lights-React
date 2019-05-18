dfcd1d68787a14068db41a156e6e8d8f2b6a0028
Santhosh <S530666@nwmissouri.edu>
2019-05-17 20:47:38 -0500

    Committed code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  
+  state = {
+    colors : [
+      {id:1,c:'red',turn:'off'},
+      {id:2,c:'green',turn:'off'},
+      {id:3,c:'yellow',turn:'off'}
+    ]
+  }
+
+  onClickListener= (key) =>{
+    this.state.colors.map(color => {
+      color.turn = 'off'
+    })
+    const index = this.state.colors.findIndex(i => {
+      return i.id === key;
+    })
+    const color = {
+      ...this.state.colors[index]
+    };
+    color.turn = 'on';
+    const colors = [...this.state.colors];
+    colors[index] = color;
+    this.setState({colors: colors})
+  }
+   
+  render(){
+    
+    let change = this.state.colors.map((color,key) => {
+      key = color.id
+      if(color.turn==='on'){
+      return(
+        <div key={color.id} id={color.c} onClick={()=>{this.onClickListener(key)}}>{color.c}</div>
+      )
+      } else {
+        return(
+          <div key={color.id} id="common" onClick={()=>{this.onClickListener(key)}}>{color.c}</div>
+        )
+      }
+    })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {change}
     </div>
   );
+}
 }
 
 export default App;
